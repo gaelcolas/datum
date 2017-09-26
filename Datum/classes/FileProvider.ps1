6d468f7b7b6c0f50b1a7ba27c43a00ea35bba3c7
gaelcolas <gaelcolas@users.noreply.github.com>
2017-09-26 10:22:23 +0100

    Updating FileProvider for recursive Datum containers

--- Datum/classes/FileProvider.ps1
+++ Datum/classes/FileProvider.ps1
@@ -8,7 +8,7 @@ Class FileProvider {
         
         $Result = Get-ChildItem $path | ForEach-Object {
             if($_.PSisContainer) {
-                $val = [scriptblock]::Create("[FileProvider]::new(`"$($_.FullName)`")")
+                $val = [scriptblock]::Create("New-DatumFileProvider -Path `"$($_.FullName)`" -DataOptions `$this.DataOptions")
                 $this | Add-Member -MemberType ScriptProperty -Name $_.BaseName -Value $val
             }
             else {
@@ -19,5 +19,5 @@ Class FileProvider {
     }
 }
 
-#$ConfigurationData = [fileProvider]::new($PWD.Path)
+#$ConfigurationData = [fileProvider]::new($PWD.Path,@{})
 #($ConfigurationData.AllNodes.psobject.Properties | % { $ConfigurationData.AllNodes.($_.Name) })[1]
