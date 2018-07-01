bcf3be764e5a909d1cefa2766734d7d647933ed6
Raimund Andée [MSFT] <r.andree@live.com>
2018-07-01 16:21:24 +0200

    Added support for DSC configurations that do not take parameters and additional support for folders

--- Datum/ScriptsToProcess/Get-DscSplattedResource.ps1
+++ Datum/ScriptsToProcess/Get-DscSplattedResource.ps1
@@ -14,12 +14,25 @@ function Global:Get-DscSplattedResource {
         $NoInvoke
     )
     # Remove Case Sensitivity of ordered Dictionary or Hashtables
-    $Properties = @{}+$Properties
+    if ($Properties)
+    {
+        $Properties = @{}+$Properties
+
+        $keys = New-Object object[]($Properties.Keys.Count)
+        $Properties.Keys.CopyTo($keys, 0)
+
+        foreach ($key in ($keys | Where-Object { $_ -like '__DatumInternal_*' }))
+        {
+            $Properties.Remove($key)
+        }
+    }
 
     $stringBuilder = [System.Text.StringBuilder]::new()
-    $null = $stringBuilder.AppendLine("Param([hashtable]`$Parameters)")
+    $null = $stringBuilder.AppendLine('Param([hashtable]$Parameters)')
     $null = $stringBuilder.AppendLine()
-    $null = $stringBuilder.AppendLine(" `$(`$Parameters=@{}+`$Parameters)")
+    $null = $stringBuilder.AppendLine(' if ($Parameters) {')
+    $null = $stringBuilder.AppendLine(' $($Parameters=@{}+$Parameters)')
+    $null = $stringBuilder.AppendLine(' }')
     $null = $stringBuilder.AppendLine(" $ResourceName $ExecutionName { ")
     foreach($PropertyName in $Properties.keys) {
         $null = $stringBuilder.AppendLine("$PropertyName = `$(`$Parameters['$PropertyName'])")
@@ -31,8 +44,13 @@ function Global:Get-DscSplattedResource {
         [scriptblock]::Create($stringBuilder.ToString())
     }
     else {
-        [scriptblock]::Create($stringBuilder.ToString()).Invoke($Properties)
+        if ($Properties) {
+            [scriptblock]::Create($stringBuilder.ToString()).Invoke($Properties)
+        }
+        else {
+            [scriptblock]::Create($stringBuilder.ToString()).Invoke()
+        }
     }
 }
+
 Set-Alias -Name x -Value Get-DscSplattedResource -scope Global
-#Export-ModuleMember -Alias x
