fb1cb2bfc7538092381ddc2f65502a6fa1efc4ae
Raimund Andree <r.andree@live.com>
2022-03-15 21:29:30 +0100

    Removed redundant calls

--- source/Public/New-DatumStructure.ps1
+++ source/Public/New-DatumStructure.ps1
@@ -133,8 +133,6 @@ function New-DatumStructure
             $module = Import-Module $storeProviderModule -Force -ErrorAction Stop -PassThru
         }
         $moduleName = ($module | Where-Object { $_.ExportedCommands.Keys -match 'New-Datum(\w+)Provider' }).Name
-        $moduleName = ($module | Where-Object { $_.ExportedCommands.Keys -match 'New-Datum(\w+)Provider' }).Name
-        $moduleName = ($module | Where-Object { $_.ExportedCommands.Keys -match 'New-Datum(\w+)Provider' }).Name
 
         $newProviderCmd = Get-Command ('{0}\New-Datum{1}Provider' -f $moduleName, $storeProviderName)
 
