8246635a0673942c0bd58393ba24f775f540f070
Raimund Andree <r.andree@live.com>
2022-03-23 18:38:44 +0100

    Added tests for 'Expand-RsopHashtable'

---
+++ tests/Integration/Expand-RsopHashtable.tests.ps1
@@ -0,0 +1,75 @@
+$here = $PSScriptRoot
+
+Import-Module -Name datum -Force
+
+InModuleScope -ModuleName Datum {
+
+    Describe 'Expand-RsopHashtable' {
+
+        $testCases = @(
+            @{
+                InputObject         = @{
+                    Key1 = 'Value1' | Add-Member -Name __File -MemberType NoteProperty -Value .\assets\DscWorkshopConfigData\Datum.yml -PassThru
+                    Key2 = ('Value1' | Add-Member -Name __File -MemberType NoteProperty -Value .\assets\DscWorkshopConfigData\Datum.yml -PassThru)
+                    Key3 = @(
+                        @{
+                            SubKey1 = ('Value1' | Add-Member -Name __File -MemberType NoteProperty -Value .\assets\DscWorkshopConfigData\Datum.yml -PassThru)
+                            SubKey2 = ('Value1' | Add-Member -Name __File -MemberType NoteProperty -Value .\assets\DscWorkshopConfigData\Datum.yml -PassThru)
+                        }
+                    )
+                }
+                IsArrayValue        = $false
+                NoSourceInformation = $false
+                Depth               = 8
+                RelativeFilePath    = 'DscWorkshopConfigData\Datum'
+            }
+            @{
+                InputObject         = @{
+                    Key1 = 'Value1' | Add-Member -Name __File -MemberType NoteProperty -Value .\assets\DscWorkshopConfigData\Datum.yml -PassThru
+                    Key2 = ('Value1' | Add-Member -Name __File -MemberType NoteProperty -Value .\assets\DscWorkshopConfigData\Datum.yml -PassThru)
+                    Key3 = @(
+                        @{
+                            SubKey1 = ('Value1' | Add-Member -Name __File -MemberType NoteProperty -Value .\assets\DscWorkshopConfigData\Datum.yml -PassThru)
+                            SubKey2 = ('Value1' | Add-Member -Name __File -MemberType NoteProperty -Value .\assets\DscWorkshopConfigData\Datum.yml -PassThru)
+                        }
+                    )
+                }
+                IsArrayValue        = $false
+                NoSourceInformation = $true
+                Depth               = 8
+                RelativeFilePath    = 'DscWorkshopConfigData\Datum'
+            }
+        )
+
+        It "'Expand-RsopHashtable' returns IDictionary objects'." -TestCases $testCases {
+            param ($InputObject, $IsArrayValue, $NoSourceInformation, $Depth)
+
+            $result = Expand-RsopHashtable -InputObject $InputObject -Depth $Depth -IsArrayValue:$IsArrayValue -NoSourceInformation:$NoSourceInformation
+
+            $result | Should -BeOfType [System.Collections.IDictionary]
+        }
+
+        It "Items in result end with RelativeFilePath if 'NoSourceInformation' is set to '<NoSourceInformation>'." -TestCases $testCases {
+            param ($InputObject, $IsArrayValue, $NoSourceInformation, $Depth, $RelativeFilePath)
+
+            $result = Expand-RsopHashtable -InputObject $InputObject -Depth $Depth -IsArrayValue:$IsArrayValue -NoSourceInformation:$NoSourceInformation
+            foreach ($key in $result.Keys)
+            {
+                if ($result.$key -isnot [array] -and $result.$key -isnot [System.Collections.IDictionary])
+                {
+                    if ($NoSourceInformation) {
+                        $result.$key | Should -Not -BeLike "*$RelativeFilePath"
+                    }
+                    else
+                    {
+                        $result.$key | Should -BeLike "*$RelativeFilePath"
+                    }
+                }
+            }
+        }
+
+
+
+    }
+
+}
