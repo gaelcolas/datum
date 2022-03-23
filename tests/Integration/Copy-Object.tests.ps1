d766af5f2ff50a27c6628e3b4f0b74c6e699da52
Raimund Andree <r.andree@live.com>
2022-03-23 21:12:10 +0100

    Added tests for 'Copy-Object'

---
+++ tests/Integration/Copy-Object.tests.ps1
@@ -0,0 +1,40 @@
+$here = $PSScriptRoot
+
+Import-Module -Name datum -Force
+
+InModuleScope -ModuleName Datum {
+
+    Describe 'Copy-Object' {
+
+        $testCases = @(
+            @{
+                DeepCopyObject = Get-Item -Path .\assets\DscWorkshopConfigData\Datum.yml
+                SourceType = 'System.IO.FileInfo'
+                TargetType = 'Deserialized.System.IO.FileInfo'
+            }
+            @{
+                DeepCopyObject = Get-Item -Path .\assets\DscWorkshopConfigData
+                SourceType = 'System.IO.DirectoryInfo'
+                TargetType = 'Deserialized.System.IO.DirectoryInfo'
+            }
+        )
+
+        It "TargetType of result object is '<TargetType>'" -TestCases $testCases {
+            param ($DeepCopyObject, $SourceType, $TargetType)
+
+            $result = Copy-Object -DeepCopyObject $DeepCopyObject
+            $DeepCopyObject -is [System.Type]$SourceType
+            ($result | Get-Member).TypeName | Select-Object -Unique | Should -Be $TargetType
+        }
+
+        It "Source and cloned objecct have the same property count" -TestCases $testCases {
+            param ($DeepCopyObject, $SourceType, $TargetType)
+
+            $result = Copy-Object -DeepCopyObject $DeepCopyObject
+
+            ($DeepCopyObject | Get-Member -MemberType Properties).Count | Should -Be ($result | Get-Member -MemberType Properties).Count
+        }
+
+    }
+
+}
