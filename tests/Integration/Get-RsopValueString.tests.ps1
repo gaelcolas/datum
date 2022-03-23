606a85211155ca47574c3cc5b47825e461d68a7b
Raimund Andree <r.andree@live.com>
2022-03-23 18:38:01 +0100

    Added tests for 'Get-RsopValueString'

---
+++ tests/Integration/Get-RsopValueString.tests.ps1
@@ -0,0 +1,45 @@
+$here = $PSScriptRoot
+
+Import-Module -Name datum -Force
+
+InModuleScope -ModuleName Datum {
+
+    Describe 'Get-RsopValueString' {
+
+        $testCases = @(
+            @{
+                InputString         = 'Hello World' | Add-Member -Name __File -MemberType NoteProperty -Value .\assets\DscWorkshopConfigData\Datum.yml -PassThru
+                Depth               = 0
+                Key                 = 'SomeKey'
+                RelativeFilePath    = 'DscWorkshopConfigData\Datum'
+                NoSourceInformation = $false
+            }
+            @{
+                InputString         = 'Hello World' | Add-Member -Name __File -MemberType NoteProperty -Value .\assets\DscWorkshopConfigData\Datum.yml -PassThru
+                Depth               = 0
+                Key                 = 'SomeKey'
+                RelativeFilePath    = 'DscWorkshopConfigData\Datum'
+                NoSourceInformation = $true
+            }
+        )
+
+        It "Result string for '<InputString>' should match the expectations'." -TestCases $testCases {
+            param ($InputString, $Depth, $Key, $IsArrayValue, $NoSourceInformation, $RelativeFilePath)
+
+            $resultString = Get-RsopValueString -InputString $InputString -Key $Key -Depth $Depth -IsArrayValue:$IsArrayValue -NoSourceInformation:$NoSourceInformation
+            $resultString | Should -BeLike "$InputString*"
+            if (-not $NoSourceInformation)
+            {
+                $resultString | Should -BeLike "*$RelativeFilePath"
+            }
+            else
+            {
+                $resultString | Should -Not -BeLike "*$RelativeFilePath"
+            }
+
+            $resultString | Should -BeOfType [string]
+        }
+
+    }
+
+}
